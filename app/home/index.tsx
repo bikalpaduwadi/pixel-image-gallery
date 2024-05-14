e11b315dbf0c06d08b6167d92d927e5f5c44f47d
Bikalpa Duwadi <bikalpa.duwadi@gmail.com>
2024-05-14 22:20:22 +0545

    Add home page with header implementation

---
+++ app/home/index.tsx
@@ -0,0 +1,113 @@
+import { theme } from "@/constants/theme";
+import { hp, wp } from "@/helper/common";
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const searchInputRef = useRef(null);
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 10 : 30;
+
+  return (
+    <View style={[styles.container, { paddingTop }]}>
+      {/* header */}
+      <View style={styles.header}>
+        <Pressable onPress={() => router.push("/")}>
+          <Text style={styles.title}>Gallery</Text>
+        </Pressable>
+        <Pressable>
+          <FontAwesome6
+            name="bars-staggered"
+            size={22}
+            color={theme.colors.neutral(0.7)}
+          ></FontAwesome6>
+        </Pressable>
+      </View>
+      <ScrollView contentContainerStyle={{ gap: 15 }}>
+        {/* search bar */}
+        <View style={styles.searchBar}>
+          <View style={styles.searchIcon}>
+            <Feather
+              name="search"
+              size={24}
+              color={theme.colors.neutral(0.4)}
+            />
+          </View>
+          <TextInput
+            value={search}
+            ref={searchInputRef}
+            onChangeText={(value) => setSearch(value)}
+            placeholder="Search images..."
+            style={styles.searchInput}
+          />
+          {search && (
+            <Pressable onPress={() => setSearch("")} style={styles.closeIcon}>
+              <Ionicons
+                name="close"
+                size={24}
+                color={theme.colors.neutral(0.6)}
+              />
+            </Pressable>
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 15,
+  },
+  header: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.semibold as any,
+    color: theme.colors.neutral(0.9),
+  },
+  searchBar: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: theme.colors.grayBG,
+    backgroundColor: theme.colors.white,
+    padding: 6,
+    paddingLeft: 10,
+    borderRadius: theme.radius.lg,
+  },
+  searchIcon: {
+    padding: 8,
+  },
+  searchInput: {
+    flex: 1,
+    borderRadius: theme.radius.sm,
+    paddingVertical: 10,
+    fontSize: hp(1.8),
+  },
+  closeIcon: {
+    backgroundColor: theme.colors.neutral(0.1),
+    padding: 8,
+    borderRadius: theme.radius.sm,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,115 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+
+import { hp, wp } from "@/helper/common";
+import { theme } from "@/constants/theme";
 
 const WelcomeScreen = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Hey there !!!</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      ></Image>
+      {/* linear gradient */}
+      <Animated.View entering={FadeInDown.duration(1000)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "white",
+            "white",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+
+        {/* content */}
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(700).springify()}
+            style={styles.title}
+          >
+            Image Gallery
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(1000).springify()}
+            style={styles.description}
+          >
+            Experience Captivating Imagery In Our Gallery
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(1200).springify()}>
+            <Pressable
+              onPress={() => router.push("home")}
+              style={styles.exploreButton}
+            >
+              <Text style={styles.exploreText}>Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
     </View>
   );
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: "absolute",
+  },
+  contentContainer: {
+    flex: 1,
+    gap: 14,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  title: {
+    fontSize: hp(5),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold as any,
+  },
+  description: {
+    fontSize: hp(2),
+    // display: "flex",
+    alignItems: "center",
+    // justifyContent: "center",
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium as any,
+  },
+  exploreButton: {
+    padding: 15,
+    marginBottom: 50,
+    paddingHorizontal: 90,
+    borderCurve: "continuous",
+    borderRadius: theme.radius.xl,
+    backgroundColor: theme.colors.neutral(0.9),
+  },
+  exploreText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium as any,
+    letterSpacing: 1,
+  },
+});
